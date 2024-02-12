2f08de6704c5be65a31e419dc0e6b329c79cde46
Niraj-Echocode <niraj.echocode@gmail.com>
2024-02-12 15:44:11 +0530

    routes added

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -1,10 +1,15 @@
-import express from 'express';
-import { authenticateUser } from '../../middleware/authorization';
+import express from "express";
+import { authenticateUser } from "../../middleware/authorization";
+import { deleteClient, listClient } from "./get";
+import { createClient, updateClient } from "./post";
 
 const clientRouter = express.Router();
 
 clientRouter.use(authenticateUser);
 
-clientRouter.get('/list', listClient);
+clientRouter.get("/list", listClient);
+clientRouter.get("/delete", deleteClient);
+clientRouter.post("/create", createClient);
+clientRouter.post("/update", updateClient);
 
 export default clientRouter;
