f3cb49e761105f69eb18f51d5da19c963f475680
Atharva Belge <atharvab.echocode@gmail.com>
2024-02-27 16:10:05 +0530

    Transaction list API Created

--- src/routes/wallet/get.js
+++ src/routes/wallet/get.js
@@ -1,53 +1,52 @@
 import { StatusCodes } from 'http-status-codes';
-import { ValidationError } from 'webpack';
-import { CustomError } from '../../helpers/custome.error';
-import { responseGenerators } from "../../lib/utils";
+import Joi from "joi";
 import WalletTransactionModel from '../../models/walletTransaction.js';
+import { responseGenerators } from "../../lib/utils";
+import { CustomError } from '../../helpers/custome.error';
 
-export const listWalletTransactions = async (req, res) => { 
-try {
-    const { type, source } = req.query;
-    const {clientId} = req.session.user.clientId;
+const { ValidationError } = Joi;
 
-    // Construct filter object based on provided query parameters
+import walletTransactionValidation from '../../helpers/validations/wallet.validation.js';
+
+export const listWalletTransactions = async (req, res) => {
+  try {
+    const querySchema = Joi.object({
+      type: Joi.string().optional(),
+      source: Joi.string().optional(),
+    });
+
+    const { error: queryError } = querySchema.validate(req.query);
+    if (queryError) {
+      throw new ValidationError(queryError.details.map(detail => detail.message).join(', '));
+    }
+
+    // Validate request body against walletTransactionValidation schema
+    await walletTransactionValidation.validateAsync(req.body);
+
+    const clientId = req?.session?.clientId;
+
     const filters = {
       ...(clientId && { clientId }),
-      ...(type && { type }),
-      ...(source && { source }),
+      ...(req.query.type && { type: req.query.type }),
+      ...(req.query.source && { source: req.query.source }),
     };
 
-    // Find wallet transactions based on filters
     const walletTransactions = await WalletTransactionModel.find(filters)
       .lean()
       .exec();
 
     return res.status(StatusCodes.OK).send(
-      responseGenerators(
-        { walletTransactions },
-        StatusCodes.OK,
-        'Success',
-        0
-      )
+      responseGenerators({ walletTransactions }, StatusCodes.OK, 'Success', 0)
     );
   } catch (error) {
     if (error instanceof ValidationError || error instanceof CustomError) {
       return res
         .status(StatusCodes.BAD_REQUEST)
-        .send(
-          responseGenerators({}, StatusCodes.BAD_REQUEST, error.message, 1)
-        );
+        .send(responseGenerators({}, StatusCodes.BAD_REQUEST, error.message, 1));
     }
     console.log(JSON.stringify(error));
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send(
-        responseGenerators(
-          {},
-          StatusCodes.INTERNAL_SERVER_ERROR,
-          "Internal Server Error",
-          1
-        )
-      );
+      .send(responseGenerators({}, StatusCodes.INTERNAL_SERVER_ERROR, "Internal Server Error", 1));
   }
 }
-
--- src/routes/wallet/index.js
+++ src/routes/wallet/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { listWalletTransactions } from './get';
+import { onlyAdminAndClientWithRoles } from '../../middleware/onlyClientAndAdmin';
 const walletRouter = express.Router();
 
-walletRouter.get('/list', listWalletTransactions);
+walletRouter.get('/list', onlyAdminAndClientWithRoles(["ADMIN", "OPERATION"]), listWalletTransactions);
 
 
 export default walletRouter;
