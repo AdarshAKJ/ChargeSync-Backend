7dace8055627b54b5376270059f57707ead809c2
Atharva Belge <atharvab.echocode@gmail.com>
2024-03-02 16:36:57 +0530

    get-single-client added

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { deleteClient, listClient } from "./get";
-import { createClient, updateClient } from "./post";
+import { createClient, getSingleClientHandler, updateClient } from "./post";
 import { onlyAdmin } from "../../middleware/onlyAdmin";
 
 const clientRouter = express.Router();
@@ -10,5 +10,6 @@ clientRouter.post("/create", onlyAdmin, createClient); // DONE
 clientRouter.get("/list", onlyAdmin, listClient);
 clientRouter.get("/delete/:id", onlyAdmin, deleteClient);
 clientRouter.post("/update/:id", onlyAdmin, updateClient);
+clientRouter.post("/single-client/:id", onlyAdmin, getSingleClientHandler);
 
 export default clientRouter;
