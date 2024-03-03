638f36267c30f753b990416f04a682b932e92366
EchocodeDev <156083229+EchocodeDev@users.noreply.github.com>
2024-03-03 15:45:41 +0530

    Mesage API updayed

--- src/routes/clientUsers/get.js
+++ src/routes/clientUsers/get.js
@@ -8,22 +8,24 @@ import { ValidationError } from "joi";
 import { singleClientUserValidation } from "../../helpers/validations/client.user.validation";
 
 export const getSingleClientUser = async (req, res) => {
-    try {
+  try {
     await singleClientUserValidation.validateAsync(req.body);
-      checkClientIdAccess(req.session, req.body.clientId);
-  
-      const { id } = req.params;
-  
-      const user = await ClientUserModel.findOne({
-        _id: id,
-        isDeleted: false,
-      });
-  
-      if (!user) {
-        throw new CustomError(`User not found.`);
-      }
-  
-      return res.status(StatusCodes.OK).send(
+    checkClientIdAccess(req.session, req.body.clientId);
+
+    const { id } = req.params;
+
+    const user = await ClientUserModel.findOne({
+      _id: id,
+      isDeleted: false,
+    });
+
+    if (!user) {
+      throw new CustomError(`User not found.`);
+    }
+
+    return res
+      .status(StatusCodes.OK)
+      .send(
         responseGenerators(
           { user },
           StatusCodes.OK,
@@ -31,24 +33,24 @@ export const getSingleClientUser = async (req, res) => {
           0
         )
       );
-    } catch (error) {
-        if (error instanceof ValidationError || error instanceof CustomError) {
-          return res
-            .status(StatusCodes.BAD_REQUEST)
-            .send(
-              responseGenerators({}, StatusCodes.BAD_REQUEST, error.message, 1)
-            );
-        }
-        console.log(JSON.stringify(error));
-        return res
-          .status(StatusCodes.INTERNAL_SERVER_ERROR)
-          .send(
-            responseGenerators(
-              {},
-              StatusCodes.INTERNAL_SERVER_ERROR,
-              "Internal Server Error",
-              1
-            )
-          );
-      }
-  };
+  } catch (error) {
+    if (error instanceof ValidationError || error instanceof CustomError) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .send(
+          responseGenerators({}, StatusCodes.BAD_REQUEST, error.message, 1)
+        );
+    }
+    console.log(JSON.stringify(error));
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(
+        responseGenerators(
+          {},
+          StatusCodes.INTERNAL_SERVER_ERROR,
+          "Internal Server Error",
+          1
+        )
+      );
+  }
+};
